db0e47536a7e4853be826157b1f1ee71fe9fbaac
Vishnu Mohan <vishnumohan.uckoo@thalesgroup.com>
2024-03-18 18:14:45 +0530

    add unit tests randomize locations start stop updating locations add logging

--- MapDemo/Models/SoldierService.cs
+++ MapDemo/Models/SoldierService.cs
@@ -1,35 +1,97 @@
-﻿using System;
+﻿using MapDemo.Events;
+using MapDemo.ViewModels;
+using NLog;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Linq.Expressions;
 using System.Timers;
 
 namespace MapDemo.Models
 {
     public class SoldierService
     {
+        private Timer dataTimer;
+        public event LocationUpdateEvent LocationUpdated;
+
         public List<SoldierCache> SoldierCache;
+        private ILogger logger;
+
         public SoldierService()
         {
+            logger = LogManager.GetLogger(nameof(SoldierService));
+
             // Initialize cache
             SoldierCache = new List<SoldierCache>();
 
+            // Set up timer to update location periodically
+            dataTimer = new Timer();
+            dataTimer.Interval = 1000; // Update every 1 second
+            dataTimer.Elapsed += UpdateLocation;
+            
         }
 
         public List<SoldierCache> Generate(int count)
         {
-            // Simulate soldier data by generating random locations
-            Random rand = new Random();
-            for (int i = 0; i < count; i++)
+            try{
+                // Simulate soldier data by generating random locations
+                Random rand = new Random();
+                for (int i = 0; i < count; i++)
+                {
+                    double latitude = 46.950249 + (rand.NextDouble() - 0.4) * 0.01;
+                    double longitude = 7.415634 + (rand.NextDouble() - 0.4) * 0.01;
+                    SoldierCache.Add
+                        (new SoldierCache(latitude, longitude) { Id = i, Name = "Name" + i, Rank = "Rank" + i });
+                }
+                logger.Info($"generated {count} soldiers");
+            }catch(Exception ex)
             {
-                double latitude = 46.950249 + (rand.NextDouble() - 0.4) * 0.01;
-                double longitude = 7.415634 + (rand.NextDouble() - 0.4) * 0.01;
-                SoldierCache.Add
-                    (new SoldierCache(latitude, longitude) { Id = i, Name = "Name" + i, Rank = "Rank" + i });
+                logger.Error($"error while generating {count} soldiers" + ex.Message);
             }
+
             return SoldierCache;
         }
+
+
+        #region DataTimer
+        public void Start()
+        {
+            dataTimer.Start();
+        }
+
+        public void Stop()
+        {
+            dataTimer.Stop();
+        }
+
+        private void UpdateLocation(object sender, ElapsedEventArgs e)
+        {
+            //TODO: Optimize this by randomizing only few checking what has changed and only update the changed items
+            RandomLocation();
+
+            LocationUpdated?.Invoke(SoldierCache);
+        }
+
+        private void RandomLocation()
+        {
+            try
+            {
+                if(SoldierCache.Count == 0)
+                {
+                    //since we're emulating
+                    Generate(10);
+                }
+
+                Random rand = new Random();
+                foreach (var soldier in SoldierCache)
+                {
+                    soldier.Location.Latitude = soldier.Location.Latitude + (rand.NextDouble() - 0.5) * 0.01;
+                    soldier.Location.Longitude = soldier.Location.Longitude + (rand.NextDouble() - 0.5) * 0.01;
+                }
+            }catch(Exception ex) {
+                logger.Error("error while update location " + ex.Message);
+            }
+        }
+        #endregion
 
     }
 }
--- MapDemo/ViewModels/MainViewModel.cs
+++ MapDemo/ViewModels/MainViewModel.cs
@@ -1,26 +1,109 @@
 ﻿using DevExpress.Mvvm;
 using MapDemo.Models;
+using NLog;
+using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Timers;
+using System.Windows.Input;
 
 namespace MapDemo.ViewModels
 {
     public class MainViewModel : ViewModelBase
     {
+        #region private properties
         private SoldierService service;
+        private Timer updateTimer;
+        private List<SoldierCache> soldierCache;
+        private ILogger logger;
+        #endregion
 
+        #region commands
+        public ICommand GenerateCommand => new DelegateCommand(GenerateSoldiers,CanGenerate);
+        public ICommand StartServiceCommand => new DelegateCommand(StartService, CanStart);
+        public ICommand StopServiceCommand => new DelegateCommand(StopService, CanStop);
+
+        #endregion
+
+        #region public properties
+        public bool IsStarted { get; set; } = false;
         public ObservableCollection<SoldierCache> Soldiers { get; set; }
+
+        #endregion
+
         public MainViewModel()
         {
-            service = new SoldierService();
+            logger = LogManager.GetLogger(nameof(MainViewModel));
 
+            service = new SoldierService();
+            service.LocationUpdated += Service_LocationUpdated;
+            IsStarted = false;
+
+            // Set up timer to update soldiers periodically
+            updateTimer = new Timer();
+            updateTimer.Interval = 2000; // Update every 1 second
+            updateTimer.Elapsed += UpdateSoldiers;
+            updateTimer.Start();
+
+        }
+
+        private bool CanGenerate()
+        {
+            return IsStarted == false;
+        }
+        private bool CanStart()
+        {
+            return IsStarted == false;
+        }
+        private bool CanStop()
+        {
+            return IsStarted == true;
         }
 
         // generating random locations
         public void GenerateSoldiers()
         {
             Soldiers = new ObservableCollection<SoldierCache>(service.Generate(10));
+            StartService();
         }
 
+
+        private void Service_LocationUpdated(List<SoldierCache> soldiers)
+        {
+            soldierCache = soldiers;
+        }
+
+        private void StartService()
+        {
+            service.Start();
+            IsStarted = true;
+        }
+
+        private void StopService()
+        {
+            service.Stop();
+            IsStarted = false;
+        }
+
+
+        private void UpdateSoldiers(object sender, ElapsedEventArgs e)
+        {
+            if (soldierCache == null) { return; }
+
+            try
+            {
+                // Update soldier location in batches
+                App.Current.Dispatcher.Invoke(() =>
+                {
+                    Soldiers = new ObservableCollection<SoldierCache>(soldierCache);
+                    RaisePropertiesChanged(nameof(Soldiers));
+                });
+
+            }catch(Exception ex)
+            {
+                logger.Error("unable to update " + ex.Message);
+            }
+        }
 
     }
 }
